4dd9505c74fb98cd9da93d3b63ee6fc110c82586
chinnadurai06 <ssdurai06@gmail.com>
2025-09-18 20:32:27 +0530

    first commit

---
+++ Q4.java
@@ -0,0 +1,9 @@
+public class Q4 {
+    public static void main(String[] args) {
+        int arr[]={10,20,30,40,50,60};
+        for(int i=0;i<arr.length;i+=2) {   //even index position
+
+            System.out.println( " " + arr[i]);
+        }
+    }
+}
